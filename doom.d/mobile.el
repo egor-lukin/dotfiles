7ed56bca3ded0c6606c848ab6b639bdbba46aa45
Egor Lukin <mail@egorlukin.me>
2026-06-16 14:47:01 +0300

    disable line numbers globaly for mobile

--- doom.d/mobile.el
+++ doom.d/mobile.el
@@ -1,7 +1,7 @@
 (setq org-agenda-prefix-format
       '((agenda . " ")))
 
-(display-line-numbers-mode -1)
+(global-display-line-numbers-mode -1)
 
 (require 'org-mobile-mode)
 (add-hook 'org-mode-hook #'org-mobile-mode)
