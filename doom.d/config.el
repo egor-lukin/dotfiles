ce468ef493816cbb6639325b8ac1a9f624dc188c
Egor Lukin <mail@egorlukin.me>
2026-09-08 11:28:18 +0300

    update gptel config

--- doom.d/config.el
+++ doom.d/config.el
@@ -335,33 +335,30 @@ If none are selected, symmetric encryption will be performed.")))
 (use-package! gptel
   :config
   (setq gptel-default-mode 'org-mode
-        gptel-model 'deepseek/deepseek-v4-flash)
+        gptel-model 'openrouter/free)
 
- (gptel-make-ollama "Ollama"
-   :host "localhost:11434"
-   :stream t
-   :models '((gemma4:e2b :capabilities (media tool-use) :mime-types ("image/jpeg"))
-             (hf.co/unsloth/gemma-4-E2B-it-qat-mobile-GGUF:UD-Q2_K_XL :capabilities (media tool-use) :mime-types ("image/jpeg"))))
+  (gptel-make-ollama "Ollama"
+    :host "localhost:11434"
+    :stream t
+    :models '((gemma4:e2b :capabilities (media tool-use) :mime-types ("image/jpeg"))
+              (hf.co/unsloth/gemma-4-E2B-it-qat-mobile-GGUF:UD-Q2_K_XL :capabilities (media tool-use) :mime-types ("image/jpeg"))))
 
-(gptel-make-openai "OpenRouter"
-   :host "openrouter.ai"
-   :endpoint "/api/v1/chat/completions"
-   :stream t
-   :key (lambda () (password-store-get "env/OPENROUTER_API_KEY"))
-   :models '(openrouter/auto:free
-             openai/gpt-3.5-turbo
-             z-ai/glm-5
-             openai/gpt-5.1-chat
-             openai/gpt-oss-120b
-             qwen/qwen3.6-plus
-             google/gemma-4-26b-a4b-it
-             google/gemma-4-26b-a4b-it:free
-             (google/gemma-4-26b-a4b-it :capabilities (media tool-use) :mime-types ("image/jpeg"))
-             deepseek/deepseek-v4-flash
-             deepseek/deepseek-v4-pro)))
-
-(setq gptel-default-mode 'org-mode
-      gptel-model 'deepseek/deepseek-v4-flash)
+  (gptel-make-openai "OpenRouter"
+    :host "openrouter.ai"
+    :endpoint "/api/v1/chat/completions"
+    :stream t
+    :key (lambda () (password-store-get "env/OPENROUTER_API_KEY"))
+    :models '(openrouter/free
+              openai/gpt-3.5-turbo
+              z-ai/glm-5
+              openai/gpt-5.1-chat
+              openai/gpt-oss-120b
+              qwen/qwen3.6-plus
+              google/gemma-4-26b-a4b-it
+              google/gemma-4-26b-a4b-it:free
+              (google/gemma-4-26b-a4b-it :capabilities (media tool-use) :mime-types ("image/jpeg"))
+              deepseek/deepseek-v4-flash
+              deepseek/deepseek-v4-pro)))
 
 (gptel-make-preset 'assistant
   :description "Universal assistant"
